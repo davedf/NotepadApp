0bcb10c9d94cda03937f8bb656fe11c10f0df4a6
David de Florinier <dave@deflorinier.me.uk>
2011-11-10 15:18:20 +0000

    Show options by orientation

---
+++ PadPad/PadPad/DdFPadPadPaperToChangeController.h
@@ -0,0 +1,13 @@
+//
+//  DdFPadPadPaperToChangeController.h
+//  PadPad
+//
+//  Created by David de Florinier on 10/11/2011.
+//  Copyright (c) 2011 de Florinier Consulting Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DdFPadPadPaperToChangeController : UITableViewController
+
+@end
